86cbdc4b1aea17dfa35cbdcfd682f29e39341455
muszka <zuzanna.fojtar@gmail.com>
2026-06-23 17:31:08 +0200

    zadanie 4. DZIAŁA!

--- lista6/zaawansowany_model_ryzyka.py
+++ lista6/zaawansowany_model_ryzyka.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def zaawansowany_proces_ryzyka(r_0, c, k, lam, T, alpha, tau, L):
     t = 0
     R = r_0
-    while L >= R >= 0 and t < T:
+    while R >= 0 and t < T:
         d_t = np.random.exponential(1 / lam)
         t += d_t
         if T < t:
@@ -25,28 +25,31 @@ def rysuj_R(r_0, c, k, lam, T, alpha, tau, L, nazwa_zestawu):
     R = r_0
     czasy = [0]
     kapital = [r_0]
-    while L >= R >= 0 and t < T:
+    while R >= 0 and t < T:
         d_t = np.random.exponential(1 / lam)
-        t_przed = t + d_t
-        if t_przed > T:
-            czasy.append(T)
-            kapital.append(R + c * (T - t))
+        t += d_t
+        if t > T:
+            # czasy.append(t_przed)
+            # R += c * d_t
+            # if R <= L:
+            #     kapital.append(R)
+            # elif R > L:
+            #     kapital.append(R - (c * d_t * tau))
             break
-        czasy.append(t_przed)
-        kapital.append(R + c * d_t)
-        t = t_przed
+        czasy.append(t)
         R += c * d_t
         Z = np.random.exponential(1 / k)
         Z_i = alpha * Z
         R -= Z_i
-        czasy.append(t)
-        if R > L:
-            kapital.append(R - (c * d_t) * tau)
-        elif R <= L:
+        if R <= L:
+            kapital.append(R)
+        elif R > L:
+            R -= c * d_t * tau
             kapital.append(R)
         if R <= 0:
             break
     plt.plot(czasy, kapital, label=nazwa_zestawu)
+    plt.hlines(L, 0, T, color="red", linestyle="--")
 
 if __name__ == "__main__":
     N = 2000
@@ -55,6 +58,21 @@ if __name__ == "__main__":
     b_c = 1.2
     b_lam = 1.0
     b_k = 1
+    b_tau = 0.5
+    b_L = 100
 
-    alpha = []
-    tau = []
+    alpha = [0.3, 0.6, 0.9]
+    tau = [0.3, 0.6, 0.9]
+    L = [50, 100, 500, 1000]
+
+    dane_a = {}
+
+    for a in alpha:
+        a_czasy = []
+        # for n in range(N):
+        t_ruiny = rysuj_R(b_r, b_c, b_k, b_lam, T, a, b_tau, b_L, "rozne alfy")
+        if t_ruiny is not None:
+                a_czasy.append(t_ruiny)
+        dane_a[a] = a_czasy
+
+    plt.show()
